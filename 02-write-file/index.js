259642534ee19317fb99e28b2ea80f98a9c3c110
pitbrest <sasha.petrov@tut.by>
2022-05-17 08:49:39 +0300

    some commit

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,6 +1,8 @@
+// Работает но записывает только 1 сообщение
+
 const path = require('path');
 const fs = require('fs');
-const adress = path.join(__dirname, 'text.txt');
+const process = require('process');
 
 console.log('Привет дорогой друг !!!');
 
@@ -10,46 +12,12 @@ const readline = require('readline')
     output: process.stdout
   });
 
-readline.question('Введите Ваше сообщение', text => {  
-  
-  const stream = new fs.ReadStream(text, {encoding: 'utf-8'});
+readline.question('Введите Ваше сообщение', text => {
+  const adress = path.join(__dirname, 'text.txt');
 
-  stream.on('data', () => {      
-    let data = stream.read();
-    fs.writeFile(adress, data, 'utf8', (err) => {
-      if(err || text.length === 0) {
-        throw new Error('Введите тест пожалуйста :)');
-      }
-    });			
-  });
-  
-  // stream.on('end', () => {   
-  //   console.log('Прощай дорогой друг. До скорой встречи !!!');
-  // }); 
- 
+  fs.writeFile(adress, text, () => {}); 
 });
 
-
-
-// Работает но записывает только 1 сообщение
-
-// const path = require('path');
-// const fs = require('fs');
-
-// console.log('Привет дорогой друг !!!');
-
-// const readline = require('readline')
-//   .createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   });
-
-// readline.question('Введите Ваше сообщение', text => {
-//   let textFileAdress = path.join(__dirname, 'text.txt');
-//   fs.writeFile(textFileAdress, text, 'utf8', (err) => {
-//     if(err || text.length === 0) {
-//       throw new Error('Введите тест пожалуйста :)');
-//     }
-//   });			
-//   readline.close();
-// });
+process.on('beforeExit', () => {
+  console.log('Прощай дорогой друг, до скорой встречи !');
+});
