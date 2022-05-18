302c245a3ff7d69630183852e43e968d76ccdc05
pitbrest <sasha.petrov@tut.by>
2022-05-18 20:38:22 +0300

    some commit

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,23 +1,22 @@
-// Работает но записывает только 1 сообщение
-
 const path = require('path');
 const fs = require('fs');
 const process = require('process');
 
-console.log('Привет дорогой друг !!!');
+const adress = path.join(__dirname, 'text.txt');
 
-const readline = require('readline')
-  .createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
 
-readline.question('Введите Ваше сообщение', text => {
-  const adress = path.join(__dirname, 'text.txt');
+const rl = readline.createInterface({ input, output });
 
-  fs.writeFile(adress, text, () => {}); 
+console.log('Hi all, nice to meet you !!!');
+
+rl.question('Type youre message, please\n', (answer) => { 
+  fs.writeFile(adress, answer, () => { });
 });
-
+rl.on('line', (input) => {
+  fs.appendFile(adress, '\n' + input, () => { });
+});
 process.on('beforeExit', () => {
-  console.log('Прощай дорогой друг, до скорой встречи !');
+  console.log('Good by my friend, see you later.');
 });
