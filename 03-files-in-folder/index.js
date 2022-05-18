226e4fdd26f65837289a0c3fee1b6f76e648f3e2
pitbrest <sasha.petrov@tut.by>
2022-05-18 22:17:32 +0300

    some commit

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,33 +1,37 @@
 const path = require('path');
 const fs = require('fs');
 
-const adress = path.join(__dirname, 'index.js');
+const adress = path.join(__dirname, 'secret-folder');
 
-fs.stat(adress, (error, stats) => {
-	if (error) {
-		console.log(error);
-	}
-	else {
-
-		console.log(stats.size);
-	}
-});
-
-// fs.readdir(adress, (err, files) => {
-// 	if (err) {
-// 		throw new Error('Have some error whith "fs.readdir"')
-// 	}
-// 	files.forEach(file => {
-// 		fs.stat('style.css', (err, stats) => {
-// 			if (err) {
-// 				throw new Error('Have some error whith "fs.stat" !');
-// 			}
-// 			else {
-// 				console.log(stats);
-// 			};
-// 		})
-// 	});
+// fs.stat(adress, (error, stats) => {
+//   if (error) {
+//     console.log(error);
+//   }
+//   else {
+//     console.log(stats.size);
+//   }
 // });
+
+fs.readdir(adress, (err, files) => {
+  if (err) {
+    throw new Error('Have some error whith "fs.readdir"');
+  }
+  files.forEach(file => {
+    let fileAdress = path.join(adress, file);
+
+    fs.stat(fileAdress, (error, stats) => {			
+      if (error) {
+        throw new Error('Have some error whith "fs.stat" !');
+      }
+      else {
+        if(stats.isFile()) {
+          console.log(path.parse(path.join(adress, file)).name + ' - ' + path.parse(path.join(adress, file)).ext.slice(1) + ' - ' + stats.size);
+        }        
+      }
+    });
+    //console.log(file);
+  });
+});
 
 
 
