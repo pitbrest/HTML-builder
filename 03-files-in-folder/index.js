3136b3c4d8042b1bec638a3d5a4f03b7fca83e86
pitbrest <sasha.petrov@tut.by>
2022-05-18 22:18:57 +0300

    some commit

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,14 +3,6 @@ const fs = require('fs');
 
 const adress = path.join(__dirname, 'secret-folder');
 
-// fs.stat(adress, (error, stats) => {
-//   if (error) {
-//     console.log(error);
-//   }
-//   else {
-//     console.log(stats.size);
-//   }
-// });
 
 fs.readdir(adress, (err, files) => {
   if (err) {
@@ -28,31 +20,6 @@ fs.readdir(adress, (err, files) => {
           console.log(path.parse(path.join(adress, file)).name + ' - ' + path.parse(path.join(adress, file)).ext.slice(1) + ' - ' + stats.size);
         }        
       }
-    });
-    //console.log(file);
+    });   
   });
 });
-
-
-
-//fs.stat('./style.css', (err, stats) => { console.log(stats); });
-// fs.readdir(adress, { withFileTypes: true }, (err, files) => {
-// 	if (err) {
-// 		throw new Error('Have some error');
-// 	}
-// 	for (let dirent of files) {
-// 		if (dirent.isFile()) {
-// 			console.log(path.parse(path.join(__dirname, dirent.name)).name + ' - ' + path.parse(path.join(__dirname, dirent.name)).ext.slice(1));
-// 		}
-// 	}
-
-
-// 	// let onlyFiles = files.filter(item => dirent.isFile(item))
-// 	// console.log(files);
-// 	// console.log(onlyFiles);
-// 	// files.forEach(file => console.log(path.extname(file)));
-// 	//console.log(files.map(file => stats.isFile(file)));
-// });
-
-
-// + ' - ' + fs.stat(dirent.name, (err, stats) => { stats })
