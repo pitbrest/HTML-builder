ce97581ebb46108a41c8fb9950b571e92c43cf18
pitbrest <sasha.petrov@tut.by>
2022-05-19 08:15:42 +0300

    some commit

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,21 +5,21 @@ const adress = path.join(__dirname, 'secret-folder');
 
 
 fs.readdir(adress, (err, files) => {
-  if (err) {
-    throw new Error('Have some error whith "fs.readdir"');
-  }
-  files.forEach(file => {
-    let fileAdress = path.join(adress, file);
+	if (err) {
+		throw new Error('Have some error whith "fs.readdir"');
+	}
+	files.forEach(file => {
+		let fileAdress = path.join(adress, file);
 
-    fs.stat(fileAdress, (error, stats) => {			
-      if (error) {
-        throw new Error('Have some error whith "fs.stat" !');
-      }
-      else {
-        if(stats.isFile()) {
-          console.log(path.parse(path.join(adress, file)).name + ' - ' + path.parse(path.join(adress, file)).ext.slice(1) + ' - ' + stats.size);
-        }        
-      }
-    });   
-  });
+		fs.stat(fileAdress, (error, stats) => {
+			if (error) {
+				throw new Error('Have some error whith "fs.stat" !');
+			}
+			else {
+				if (stats.isFile()) {
+					console.log(path.parse(path.join(adress, file)).name + ' - ' + path.parse(path.join(adress, file)).ext.slice(1) + ' - ' + stats.size + ' bytes');
+				}
+			}
+		});
+	});
 });
