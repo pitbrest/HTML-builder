e0840fa4d144bda2df4cc3fee620b83601ef888f
pitbrest <sasha.petrov@tut.by>
2022-05-19 15:19:09 +0300

    some commit

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,31 +3,39 @@ const fs = require('fs');
 
 const adress = path.join(__dirname, 'files-copy');
 
-
-
-fs.mkdir(adress, { recursive: true }, err => {
-	if (err) {
-		throw new Error('Have some error with fs.mkdir');
+fs.readdir(adress, (error, files) => {
+	if (error) {
+		fs.mkdir(adress, { recursive: true }, err => {
+			if (err) {
+				throw new Error('Have some error with fs.mkdir');
+			}
+		})
 	}
-})
+	else {
+		files.forEach(file => {
+			fs.unlink(path.join(adress, file), (err) => {
+				if (err) {
+					throw new Error('Have some error with fs.unlink');
+				};
+			});
+		});
+	}
+});
 
 let source = path.join(__dirname, 'files');
 let destination = path.join(__dirname, 'files-copy');
 
 fs.readdir(source, (err, files) => {
 	if (err) {
-		throw new Error('Have some error whith "fs.readdir"');
+		throw new Error('Have some error whith fs.readdir');
 	}
-	else {
-		files.forEach(file => {
-
-			fs.copyFile(path.join(source, file), path.join(destination, file), err => {
-				if (err) {
-					throw new Error('Have some error with fs.copyFile');
-				}
-			})
+	files.forEach(file => {
+		fs.copyFile(path.join(source, file), path.join(destination, file), err => {
+			if (err) {
+				throw new Error('Have some error with fs.copyFile');
+			}
 		});
-	}
+	});
 });
 
 
