d09ce2a6a5dbac0920e1e62053a2995d743352fa
pitbrest <sasha.petrov@tut.by>
2022-05-22 22:20:11 +0300

    some commit

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,25 +3,66 @@ const fs = require('fs');
 const fsPromises = require('fs/promises');
 
 const projectFolderAdress = path.join(__dirname, 'project-dist');
-const templateFileAdress = path.join(__dirname, 'template.html');
-const cssFileAdress = path.join(projectFolderAdress, 'style.css');
-const htmlFileAdress = path.join(projectFolderAdress, 'index.html');
+const templatePath = path.join(__dirname, 'template.html');
+const assetsPath = path.join(__dirname, 'assets');
+const componentsPath = path.join(__dirname, 'components');
+const cssStylesPath = path.join(__dirname, 'styles');
 
-const assetsAdress = path.join(__dirname, 'assets');
-const componentsAdress = path.join(__dirname, 'components');
-const cssStylesAdress = path.join(__dirname, 'styles');
+const newCssPath = path.join(projectFolderAdress, 'style.css');
+const newHtmlPath = path.join(projectFolderAdress, 'index.html');
 
 
-fs.mkdir(projectFolderAdress, { recursive: true }, (e) => {
-	if(e) {
-		throw new Error('Have some troubles whis "fs.mkdir"');
-	}
-});
-
-let myAsync = async () => {	
-	const fileTemplate = await fsPromises.readFile(templateFileAdress, 'utf8');
-
-	console.log( fileTemplate.split('{'));
+const createFolder = async (path) => {
+	return await 
+		fsPromises.mkdir(path, { recursive: true }, (e) => {
+			if(e) {
+				throw new Error('Have some troubles whith "mkdir"');
+			}
+		});
 };
 
-myAsync();
+const createFile = async (path, content) => {
+	return await 
+		fsPromises.writeFile(path, content, (e) => {
+			if(e) {
+				throw new Error('Have some troubles whith "writeFile"');
+			}
+		});
+};
+
+const mergeCss = async () => {
+	let resultArr = [];
+	const cssFiles = await fsPromises.readdir(cssStylesPath);
+
+	for(let file of cssFiles) {
+		let fileAdress = path.join(cssStylesPath, file);
+		let stats = await fsPromises.stat(fileAdress);
+
+		if(stats.isFile() && path.parse(fileAdress).ext.slice(1) === 'css') {
+			const cssFileContent = await fsPromises.readFile(fileAdress, 'utf-8');
+			resultArr.push(cssFileContent + '\n');
+		}
+	}
+
+	let resultContent = resultArr.join('');	
+	await createFile(newCssPath, resultContent);
+};
+
+const copyDir = async (fromDir, toDir) => {
+	await fsPromises.rm(toDir, { recursive: true, force: true });
+	await fsPromises.mkdir(toDir, { recursive: true });
+
+	const copiedItemsNames = await fsPromises.readdir(fromDir, { withFileTypes: true });
+
+	for(let item of copiedItemsNames) {
+		const currentItemPath = path.join(fromDir, item.name);
+		const copiedItemPath = path.join(toDir, item.name);
+		
+		if(item.isDirectory()) {
+			await fsPromises.mkdir(copiedItemPath, {recursive: true});
+			await copyDir(currentItemPath, copiedItemPath);
+		} else if (item.isFile()) {
+			await fsPromises.copyFile(copiedItemPath, currentItemPath);
+		}
+	}
+};
